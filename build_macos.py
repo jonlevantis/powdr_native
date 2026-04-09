636123afd7419a12023d381d25c897c732773224
jonlevantis <jonjamessmith001@gmail.com>
2026-04-09 19:07:35 -0400

    Add files via upload

--- build_macos.py
+++ build_macos.py
@@ -1,22 +1,34 @@
 """
 build_macos.py
 --------------
-Monkey-patches os.symlink to ignore FileExistsError,
-then runs PyInstaller programmatically.
+Monkey-patches os.symlink and os.makedirs to handle pre-existing
+paths during PyInstaller's COLLECT and BUNDLE steps on macOS.
 """
 import os
 import sys
 
-_original_symlink = os.symlink
-
+# Patch os.symlink
+_orig_symlink = os.symlink
 def _safe_symlink(src, dst, *args, **kwargs):
     try:
-        _original_symlink(src, dst, *args, **kwargs)
+        _orig_symlink(src, dst, *args, **kwargs)
+    except (FileExistsError, OSError):
+        pass
+os.symlink = _safe_symlink
+
+# Patch os.makedirs
+_orig_makedirs = os.makedirs
+def _safe_makedirs(path, *args, **kwargs):
+    # If a file exists at this path, remove it first then create the dir
+    if os.path.isfile(path):
+        os.remove(path)
+    try:
+        _orig_makedirs(path, *args, **kwargs)
     except FileExistsError:
         pass
+os.makedirs = _safe_makedirs
 
-os.symlink = _safe_symlink
-print("os.symlink patched to ignore FileExistsError")
+print("os.symlink and os.makedirs patched")
 
 from PyInstaller.__main__ import run
 sys.argv = ['pyinstaller', 'powdr_native.spec']
